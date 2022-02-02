8e12acf173f0a7bbf2ca8a3857a914e98f296e8c
knt800run <hknt1255@gmail.com>
2022-02-02 16:01:13 +0900

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React,{ useState } from "react";
+import React from "react";
 import "./App.css";
 
 export const App = () => {
 	return (
 		<>
-
+			<div>インプットエリア</div>
+			<div>冷蔵庫エリア</div>
+			<div>冷凍庫エリア</div>
+			<div>野菜室エリア</div>
 		</>
 	);
 }
