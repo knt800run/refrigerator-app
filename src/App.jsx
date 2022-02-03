08cc854cf1c269a4170193a1421d2dfc24d5883b
knt800run <hknt1255@gmail.com>
2022-02-03 11:29:20 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,48 @@ import "./App.css";
 export const App = () => {
 	return (
 		<>
-			<div>インプットエリア</div>
-			<div>冷蔵庫エリア</div>
-			<div>冷凍庫エリア</div>
-			<div>野菜室エリア</div>
+			<div className="input-area">
+				<input type="text" placeholder="食品名を入力" />
+				<input type="date"/>
+				<input type="text" placeholder="個数を入力" />
+				<button>追加</button>
+			</div>
+			<div className="fridge-area">
+				<p className="title">冷蔵庫エリア</p>
+				<ul>
+					<li>
+						<div>
+							<p>牛肉</p>
+							<button>編集</button>
+							<button>削除</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div className="freezer-area">
+				<p className="title">冷凍庫エリア</p>
+				<ul>
+					<li>
+						<div>
+							<p>ハーゲンダッツ</p>
+							<button>編集</button>
+							<button>削除</button>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div className="veggie-area">
+				<p className="title">野菜室エリア</p>
+				<ul>
+					<li>
+						<div>
+							<p>にんじん</p>
+							<button>編集</button>
+							<button>削除</button>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</>
 	);
 }
