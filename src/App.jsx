c1d965c1da2947491ebf1ee0fafabec7825809e7
knt800run <hknt1255@gmail.com>
2022-02-06 10:47:25 +0900

    commit until onclickadd

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,55 @@ import "./App.css";
 
 export const App = () => {
 
-	const [foodName, setFoodName] = useState('');
-	const [exp, setExp] = useState('');
-	const [qty, setQty] = useState('');
-	const [keepArea, setKeepArea] = useState('');
+	const [inputInfo, setInputInfo] = useState({
+		foodName: "",
+		exp: "",
+		qty: "",
+		keepArea: ""
+	});
 
 	const [fridgeFoods, setFridgeFoods] = useState([
-		{name: "牛乳", date: "2022-06-07", qty: "1パック"},
-		{name: "豚肉", date: "2022-03-27", qty: "200g"}
+		{foodName: "牛乳", exp: "2022-06-07", qty: "1パック", keepArea: "冷蔵庫"},
+		{foodName: "豚肉", exp: "2022-02-27", qty: "200g", keepArea: "冷蔵庫"},
 	]);
 	const [freezerFoods, setFreezerFoods] = useState([
-		{name: "ハーゲンダッツ", date: "", qty: "3個"}
+		{foodName: "ハーゲンダッツ", exp: "", qty: "3個", keepArea: "冷凍庫"},
 	]);
 	const [veggieFoods, setVeggieFoods] = useState([
-		{name: "にんじん", date: "2022-02-27", qty: "2本"}
+		{foodName: "にんじん", exp: "2022-02-07", qty: "2本", keepArea: "野菜室"},
 	]);
 
-	const onChangeFoodName = (e) => setFoodName(e.target.value);
-	const onChangeExp = (e) => setExp(e.target.value);
-	const onChangeQty = (e) => setQty(e.target.value);
-	const onChangeKeepArea = (e) => setKeepArea(e.target.value);
+	const onChange = (e) => {
+		const target = e.target;
+		const value = target.value;
+		const name = target.name;
+		setInputInfo({ ...inputInfo, [name]: value });
+	};
 
 	const onClickAdd = () => {
-		alert(foodName+exp+qty+keepArea);
+		if (inputInfo.foodName === "" || inputInfo.qty === "") return;
+		if (inputInfo.keepArea === "冷蔵庫") {
+			const newFoods = [...fridgeFoods, inputInfo];
+			setFridgeFoods(newFoods);
+		} else if (inputInfo.keepArea === "冷凍庫") {
+			const newFoods = [...freezerFoods, inputInfo];
+			setFreezerFoods(newFoods);
+		} else if (inputInfo.keepArea === "野菜室") {
+			const newFoods = [...veggieFoods, inputInfo];
+			setVeggieFoods(newFoods);
+		} else {
+			return;
+		}
 	};
 
 	return (
 		<>
 			<div className="input-area">
 				<h3>冷蔵庫管理アプリ</h3>
-				<input type="text" placeholder="食品名を入力" value={foodName} onChange={onChangeFoodName} />
-				<input type="date" value={exp} onChange={onChangeExp} />
-				<input type="text" placeholder="個数を入力" value={qty} onChange={onChangeQty} />
-				<select name="保存場所" value={keepArea} onChange={onChangeKeepArea}>
+				<input name="foodName" type="text" placeholder="食品名を入力" value={inputInfo.foodName} onChange={onChange} />
+				<input name="exp" type="date" value={inputInfo.exp} onChange={onChange} />
+				<input name="qty" type="text" placeholder="個数を入力" value={inputInfo.qty} onChange={onChange} />
+				<select name="keepArea" value={inputInfo.keepArea} onChange={onChange}>
 					<option value="" disabled>保存場所</option>
 					<option value="冷蔵庫">冷蔵庫</option>
 					<option value="冷凍庫">冷凍庫</option>
@@ -50,8 +66,8 @@ export const App = () => {
 						return (
 							<li key={index}>
 								<div className="list-row">
-									<p>{food.name}</p>
-									<p>{food.date}</p>
+									<p>{food.foodName}</p>
+									<p>{food.exp}</p>
 									<p>{food.qty}</p>
 									<button>編集</button>
 									<button>削除</button>
@@ -68,8 +84,8 @@ export const App = () => {
 						return (
 							<li key={index}>
 								<div className="list-row">
-									<p>{food.name}</p>
-									<p>{food.date}</p>
+									<p>{food.foodName}</p>
+									<p>{food.exp}</p>
 									<p>{food.qty}</p>
 									<button>編集</button>
 									<button>削除</button>
@@ -86,8 +102,8 @@ export const App = () => {
 						return (
 							<li key={index}>
 								<div className="list-row">
-									<p>{food.name}</p>
-									<p>{food.date}</p>
+									<p>{food.foodName}</p>
+									<p>{food.exp}</p>
 									<p>{food.qty}</p>
 									<button>編集</button>
 									<button>削除</button>
